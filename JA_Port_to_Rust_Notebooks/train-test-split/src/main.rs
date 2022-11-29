2751e7906cc23f58f6856d7a9a376faa5c0b840a
squeeko <squeeko@users.noreply.github.com>
2022-11-28 18:11:19 -0800

    using polars for dataframe

--- JA_Port_to_Rust_Notebooks/train-test-split/src/main.rs
+++ JA_Port_to_Rust_Notebooks/train-test-split/src/main.rs
@@ -1,18 +1,10 @@
-extern crate csv;
+use polars::prelude::*;
 
-use std::io::prelude::*;
-use std::process;
+fn main() {
+    let df = CsvReader::from_path("/Users/squeeko/AI-for-CyberSecurity/JA_Port_to_Rust_Notebooks/data/north_korea_missile_test_database.csv")
+        .unwrap()
+        .finish()
+        .unwrap();
 
-fn main() -> Result<(), csv::Error> {
-    let mut rdr = csv::Reader::from_path("/Users/squeeko/AI-for-CyberSecurity/JA_Port_to_Rust_Notebooks/data/north_korea_missile_test_database.csv")?;
-    for result in rdr.records() {
-        match result {
-            Ok(record) => println!("{:?}", record),
-            Err(err) => {
-                println!("Error reading CSV from file: {}", err);
-                process::exit(1);
-            }
-        }
-    }
-    Ok(())
+    println!("{}", df);
 }
